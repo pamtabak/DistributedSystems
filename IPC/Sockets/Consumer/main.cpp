ca9fb9eeb3894b7e154636d009c3f9edaee14939
Eric Figueiredo <eric_reisfig@poli.ufrj.br>
2016-04-24 19:36:09 -0300

    producer/consumer working

--- IPC/Sockets/Consumer/main.cpp
+++ IPC/Sockets/Consumer/main.cpp
@@ -6,6 +6,8 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+#define BUFFER_SIZE 256
+
 std::string isPrime (int number)
 {
     for (int i = 2; i <= (sqrt(number) + 1); i++)
@@ -18,7 +20,71 @@ std::string isPrime (int number)
     return "true";
 }
 
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
+    int sockFileDesc, portNo, response;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in serverAddr;
+    struct hostent *server;
+
+    if (argc < 3)
+    {
+        error((char *) "ERROR obtaining address and port");
+    }
+
+    portNo = atoi(argv[2]);
+    sockFileDesc = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockFileDesc < 0)
+    {
+        error((char *) "ERROR opening socket");
+    }
+
+    server = gethostbyname(argv[1]);
+    if(server == NULL)
+    {
+        error((char *) "ERROR no host found");
+    }
+
+    bzero((char *) &serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    bcopy((char *) server->h_addr, (char *) &serverAddr.sin_addr.s_addr, server->h_length);
+    serverAddr.sin_port = htons(portNo);
+
+    if(connect(sockFileDesc, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    {
+        error((char *) "ERROR connectiong to server");
+    }
+
+    while(true)
+    {
+        bzero(buffer, BUFFER_SIZE);
+        response = read(sockFileDesc, buffer, BUFFER_SIZE - 1);
+        if(response < 0)
+        {
+            error((char *) "ERROR reading from socket");
+        }
+
+        int n = atoi(buffer);
+        if(n == 0)
+        {
+            break;
+        }
+
+        std::string s = isPrime(n);
+        const char *c = s.c_str();
+        response = write(sockFileDesc, c, s.size());
+        if(response < 0)
+        {
+            error((char *) "ERROR writing to socket");
+        }
+    }
+    close(sockFileDesc);
+
     return 0;
 }
--- IPC/Sockets/Producer/main.cpp
+++ IPC/Sockets/Producer/main.cpp
@@ -1,30 +1,109 @@
 #include <iostream>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <random>
-#include <limits>
 
-using namespace std;
+#define RAND() rand() % std::numeric_limits<int>::max() + 1
 
-default_random_engine generator;
-uniform_int_distribution<int> distribution(1,numeric_limits<int>::max());
+#define BUFFER_SIZE 256
+#define PORT_NO 666
+#define MAX_CONNECTIONS 5
+
 
 int getIncreasingRandomNumber(int lastRandomNumber)
 {
-    int r = distribution(generator);
-    while (r <= lastRandomNumber)
+    srand(time(NULL));
+    int r = RAND();
+    while(r <= lastRandomNumber)
     {
-        r = distribution(generator);
+        r = RAND();
     }
     return r;
 }
 
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World" << std::endl;
+    int sockFileDesc, newSockFileDesc, response, indexOfZero;
+    socklen_t clientLen;
+    char buffer[BUFFER_SIZE];
+    struct sockaddr_in serverAddr, clientAddr;
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(argc != 2)
+    {
+        std::cout << "Wrong parameters." << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    sockFileDesc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if(sockFileDesc < 0)
+    {
+        error((char *) "ERROR opening socket");
+    }
+
+    // bzero() sets all values in a buffer to zero
+    bzero((char *) &serverAddr, sizeof(serverAddr));
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT_NO);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // bind() binds a socket to an address
+    if(bind(sockFileDesc, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    {
+        error((char *) "ERROR binding socket");
+    }
+
+    // listen() allows the process to listen on the socket for connections. The second argument is the
+    // number of connections that can be waiting while the process is handling a particular connection
+    listen(sockFileDesc, MAX_CONNECTIONS);
+
+    clientLen = sizeof(clientAddr);
+    // accept() causes the process to block until a client connects to the server
+    newSockFileDesc = accept(sockFileDesc, (struct sockaddr *) &clientAddr, &clientLen);
+    if(newSockFileDesc < 0)
+    {
+        error((char *) "ERROR accepting client connection");
+    }
+
+    indexOfZero = atoi(argv[1]);
+    int lastRandomNumber = 0;
+    int randomNumber;
+    for(int i = 0; i < indexOfZero; i++)
+    {
+        randomNumber = getIncreasingRandomNumber(lastRandomNumber);
+        lastRandomNumber = randomNumber;
+        std::string s = std::to_string(randomNumber);
+        const char * c = s.c_str();
+        response = write(newSockFileDesc, c, s.size());
+        if(response < 0)
+        {
+            error((char *) "ERROR writing to socket");
+        }
+
+        bzero(buffer, BUFFER_SIZE);
+        // read() blocks until there is something for it to read in the socket
+        response = read(newSockFileDesc, buffer, BUFFER_SIZE - 1);
+        if(response < 0)
+        {
+            error((char *) "ERROR reading from socket");
+        }
+        std::cout << "The number " << s << " is prime: " << buffer << std::endl;
+    }
+    response = write(newSockFileDesc, "0", 1);
+    if(response < 0)
+    {
+        error((char *) "ERROR writing to socket");
+    }
+
+    close(newSockFileDesc);
+    close(sockFileDesc);
+
 
     return 0;
 }
