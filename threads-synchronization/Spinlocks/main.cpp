274917484808c63b1bee414fd3b6ccf9a6551658
Eric Figueiredo <eric_reisfig@poli.ufrj.br>
2016-05-07 02:09:55 -0300

    creating thread and basic test

--- threads-synchronization/Spinlocks/main.cpp
+++ threads-synchronization/Spinlocks/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdint>
+#include <stdlib.h>
+#include <stdint.h>
+#include <pthread.h>
+
+struct arg_struct
+{
+	int id;
+	std::string data;
+};
 
 int8_t random(int seed, int8_t min, int8_t max)
 {
@@ -13,6 +20,15 @@ void fill(int8_t *v, long n, int seed)
 	for(long i = 0; i < n; ++i)
 	{
 		v[i] = random(seed, -100, 100);
+	}
+}
+
+void *test(void *arguments)
+{
+	struct arg_struct *args = (struct arg_struct *) arguments;
+	for(int i = 0; i < 10; ++i)
+	{
+		printf("Thread %d\n", args->id);
 	}
 }
 
@@ -32,10 +48,21 @@ int main(int argc, char const *argv[])
 	int8_t *v = (int8_t *) malloc(n * sizeof(int8_t));
 	fill(v, n, seed);
 
-	std::cout << "wainting ..." << std::endl;
-	getchar();
+	pthread_t *threads = (pthread_t *) malloc(k * sizeof(pthread_t));
+	struct arg_struct **args = (struct arg_struct **) malloc(k * sizeof(struct arg_struct *));
+	for(int i = 0; i < k; ++i)
+	{
+		args[i] = (arg_struct *) malloc(sizeof(arg_struct));
+		args[i]->id = i;
+		args[i]->data = "Hello";
+		pthread_create(&threads[i], NULL, test, (void *) args[i]);
+	}
+	for(int i = 0; i < k; ++i)
+	{
+		pthread_join(threads[i], NULL);
+	}
 
-	delete [] v;
+	delete [] v, threads, args;
 
 	return 0;
 }
